74f52997c45525227ccb31253ce90cae9a8f49b8
MRINAL GUPTA <mrinalgupta2002@gmail.com>
2022-02-26 20:33:26 +0530

    Added example activity

---
+++ app/src/main/java/com/example/ticketapp/ExampleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ticketapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+    }
+}
